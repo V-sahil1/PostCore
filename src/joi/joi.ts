84097e6267de16ad0fff9cc931c1c8668eec14e2
V-sahil1 <sahilvardekar1@gmail.com>
2026-01-30 16:05:50 +0530

    joi validation is implemented

--- src/joi/joi.ts
+++ src/joi/joi.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 
 export const createSchema =  Joi.object({
-    Username:Joi.string().min(3).max(7).required(),
+    user_name:Joi.string().min(3).max(7).required(),
     email:Joi.string().email().required(),
     password:Joi.string().min(3).max(10).required(),
     role:Joi.string().required(),
