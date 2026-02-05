887e426099b2bc615d087561bd61632c7842139c
V-sahil1 <sahilvardekar1@gmail.com>
2026-02-05 17:10:33 +0530

    swagger  implemented

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,45 @@
-import express from "express";
-import type { Application, Request, Response } from "express";
-import session from "express-session";
-import connectDB from "./config/mongo";
-import db from "./models";
-// import { login, register } from "./modules/user/useRoute";
-import passport from "./middleware/passport";
-// import  { creatpost } from "./modules/post/postRoute";
-// import { authenticateJWT } from "./middleware/jwt";
-// import { comment } from "./modules/comment/commentRoute";
-// import { saveToken } from "./modules/auth/tokenRoute";
-// import { deletecomment, getcomment, getpost, postDelete, UpdateComment, updatePost,userDelete,getpostById } from "./controller/route";
-// import validate from "./middleware/validateSchema";
-// import { createSchema } from "./joi/joi";
-// import { optionalJWT } from "./middleware/optinal";
-import morganMiddleware from "./middleware/morganLogger";
-import { captureResponse } from "./middleware/responseCapture";
-import router from "./modules/index";
+  import express from "express";
+  import type { Application, Request, Response } from "express";
+  import session from "express-session";
+  import connectDB from "./config/mongo";
+  import db from "./models";
+  import passport from "./middleware/passport";
+  import morganMiddleware from "./middleware/morganLogger";
+  import { captureResponse } from "./middleware/responseCapture";
+  import router from "./modules/index";
+  import swaggerUi from  "swagger-ui-express";
+  import swaggerDocument from "./swagger-output.json"
+  const app: Application = express();
+  const PORT = Number(process.env.PORT);
+
+  connectDB();
+  const _db = db;
+  void _db;
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morganMiddleware);
+  app.use(captureResponse);
 
 
-const app: Application = express();
-const PORT = Number(process.env.PORT);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-connectDB();
-const _db = db;
-void _db;
+  app.use(
+    session({
+      secret: String(process.env.SESSION_SECRET),
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morganMiddleware);
-app.use(captureResponse);
+  app.get("/", (_req: Request, res: Response) => {
+    res.send("Server is running");
+  });
 
-app.use(
-  session({
-    secret: String(process.env.SESSION_SECRET),
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+  app.use("/",router)
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Server is running");
-});
-
-app.use("/",router)
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
